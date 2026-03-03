12fa6651c4a4f7e53747fcfb1050d1c53bce746c
BernabeJudicael <bernabejudicael02@gmail.com>
2026-03-03 16:35:29 +0100

    mon message

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,7 +1,31 @@
 
-// npm install --save-dev prisma dotenv
+// // npm install --save-dev prisma dotenv
+// import "dotenv/config";
+// import { defineConfig, env } from "prisma/config";
+
+// export default defineConfig({
+//   schema: "prisma/schema.prisma",
+//   migrations: {
+//     path: "prisma/migrations",
+//   },
+//   engine: "classic",
+//   datasource: {
+//     url: env("DATABASE_URL"),
+//   },
+// });
+
+
+
+
+
+
+// prisma.config.ts// prisma.config.ts
 import "dotenv/config";
-import { defineConfig, env } from "prisma/config";
+import { defineConfig } from "prisma/config";
+
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL n'est pas défini dans le fichier .env");
+}
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
@@ -10,6 +34,6 @@ export default defineConfig({
   },
   engine: "classic",
   datasource: {
-    url: env("DATABASE_URL"),
+    url: process.env.DATABASE_URL,
   },
 });
